6b691c20785d9a1a30a845770a421465f162549f
dandiez <47832466+dandiez@users.noreply.github.com>
2020-12-11 20:49:40 +0100

    solved part 2

--- day11.py
+++ day11.py
@@ -3,8 +3,9 @@ import networkx as nx
 from parse import *
 import copy
 from collections import defaultdict
+import itertools
 
-with open("input11.txt") as f:
+with open("input11v.txt") as f:
     lines =[line.strip() for line in f.readlines() if line.strip()]
 
 # part 1
@@ -53,6 +54,8 @@ class Grid():
         return Grid(lines)
 
 
+
+
 g = Grid(lines)
 g.show()
 
@@ -76,9 +79,63 @@ def simulate_grid(orig_grid):
     #print(grid.show())
     return changed, grid
 
-last_changed = True
-while last_changed:
-    last_changed, g = simulate_grid(g)
+def part_1(g):
+    last_changed = True
+    while last_changed:
+        last_changed, g = simulate_grid(g)
+    print(g.count_char("#"))
 
+# part_1(g)
 
-print(g.count_char("#"))
+# part 2
+def find_next_visible_seat_in_direcion(g, x, y, dx, dy):
+    if dx==0 and dy==0:
+        return None
+    xx = x
+    yy = y
+    while (0<=xx<=g.x_max) and ( 0<=yy<=g.y_max):
+        xx +=dx
+        yy +=dy
+        if g.get(xx, yy) in [ "L", "#"]:
+            return (xx, yy)
+    return None
+
+def find_all_visible_seats(g, x, y):
+    visible = []
+    for (dx, dy) in itertools.product((-1,0,1), repeat=2):
+        seat = find_next_visible_seat_in_direcion(g, x, y, dx, dy)
+        if seat is not None:
+            visible.append(seat)
+    return visible
+
+def cache_visible_seats(g):
+    visible_seats=dict()
+    for x in range(g.x_max):
+        for y in range(g.y_max):
+            visible_seats[(x, y)] = find_all_visible_seats(g,x,y)
+    return visible_seats
+
+def solve_grid_part_2(orig_grid, visible_seats):
+    grid = orig_grid.get_copy()
+    changed = False
+    for (n,m) in itertools.product( range(grid.x_max), range(grid.y_max)):
+        adjacent = [orig_grid.get(xx,yy) for (xx,yy) in  visible_seats[(n,m)]]
+        if orig_grid.get(n,m) == "L":
+            if adjacent.count("#") == 0:
+                grid.set(n,m, "#")
+                changed = True
+        if orig_grid.get(n,m) == "#":
+            if adjacent.count("#") >=5:
+                grid.set(n,m, "L")
+                changed = True
+    print("calculation done")
+    return changed, grid
+
+def part_2(g):
+    visible_seats = cache_visible_seats(g)
+    last_changed = True
+    while last_changed:
+        last_changed, g = solve_grid_part_2(g, visible_seats)
+    print(g.count_char("#"))
+
+part_2(g)
