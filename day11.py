29b02342820fc88be95c9dda290a9193a931db6c
dandiez <47832466+dandiez@users.noreply.github.com>
2020-12-11 21:43:56 +0100

    make generic part 1 and part 2

--- day11.py
+++ day11.py
@@ -4,11 +4,8 @@ from parse import *
 import copy
 from collections import defaultdict
 import itertools
+import numpy as np
 
-with open("input11v.txt") as f:
-    lines =[line.strip() for line in f.readlines() if line.strip()]
-
-# part 1
 
 class Grid():
     def __init__(self, lines):
@@ -16,6 +13,7 @@ class Grid():
         self.x_max = len(lines)
         self.y_max = len(lines[0])
         self.pad = "."
+        self.visible = None
 
     def get(self, x, y):
         if x < 0 or y < 0:
@@ -51,91 +49,83 @@ class Grid():
 
     def get_copy(self):
         lines = copy.deepcopy(self.lines)
-        return Grid(lines)
+        new_g = Grid(lines)
+        new_g.visible = self.visible # reuse same cached list
+        return new_g
 
-
-
-
-g = Grid(lines)
-g.show()
-
-
-
-def simulate_grid(orig_grid):
-    grid = orig_grid.get_copy()
-    changed = False
-    for n in range(grid.x_max):
-        for m in range(grid.y_max):
-            adjacent = orig_grid.subgrid(n, m, 1, 1)
-            if orig_grid.get(n,m) == "L":
-                if adjacent.count_char("#") == 0:
-                    grid.set(n,m, "#")
-                    changed = True
-            if orig_grid.get(n,m) == "#":
-                if adjacent.count_char("#") >=5:
-                    grid.set(n,m, "L")
-                    changed = True
-    print("calculation done")
-    #print(grid.show())
-    return changed, grid
-
-def part_1(g):
-    last_changed = True
-    while last_changed:
-        last_changed, g = simulate_grid(g)
-    print(g.count_char("#"))
-
-# part_1(g)
-
-# part 2
-def find_next_visible_seat_in_direcion(g, x, y, dx, dy):
-    if dx==0 and dy==0:
+    def _find_next_visible_seat_in_direcion(self, x, y, dx, dy, max_distance=np.inf):
+        if dx==0 and dy==0:
+            return None
+        xx = x
+        yy = y
+        d = 0
+        while (0<=xx<=g.x_max) and ( 0<=yy<=g.y_max) and (d < max_distance):
+            xx +=dx
+            yy +=dy
+            d += 1
+            if g.get(xx, yy) in [ "L", "#"]:
+                return (xx, yy)
         return None
-    xx = x
-    yy = y
-    while (0<=xx<=g.x_max) and ( 0<=yy<=g.y_max):
-        xx +=dx
-        yy +=dy
-        if g.get(xx, yy) in [ "L", "#"]:
-            return (xx, yy)
-    return None
 
-def find_all_visible_seats(g, x, y):
-    visible = []
-    for (dx, dy) in itertools.product((-1,0,1), repeat=2):
-        seat = find_next_visible_seat_in_direcion(g, x, y, dx, dy)
-        if seat is not None:
-            visible.append(seat)
-    return visible
+    def _find_all_visible_seats(self, x, y, max_distance=np.inf):
+        visible = []
+        for (dx, dy) in itertools.product((-1,0,1), repeat=2):
+            seat = g._find_next_visible_seat_in_direcion(x, y, dx, dy, max_distance=max_distance)
+            if seat is not None:
+                visible.append(seat)
+        return visible
 
-def cache_visible_seats(g):
-    visible_seats=dict()
-    for x in range(g.x_max):
-        for y in range(g.y_max):
-            visible_seats[(x, y)] = find_all_visible_seats(g,x,y)
-    return visible_seats
+    def cache_visible_seats(self, max_distance = np.inf):
+        """Keep a dict which has all visible seats at every location of the grid {(x,y):[(x0, y0), (x1, y1),...]}"""
+        if self.visible is not None:
+            # do not recalculate cache if already done
+            return
+        visible_seats=dict()
+        for (x, y) in self.iterate_grid():
+                visible_seats[(x, y)] = g._find_all_visible_seats(x,y, max_distance=max_distance)
+        self.visible = visible_seats
 
-def solve_grid_part_2(orig_grid, visible_seats):
+    def iterate_grid(self):
+        for (n,m) in itertools.product( range(self.x_max), range(self.y_max)):
+            yield (n, m)
+
+def iterate_solve_grid(orig_grid, max_distance, occupancy_limit):
+    orig_grid.cache_visible_seats(max_distance=max_distance)
     grid = orig_grid.get_copy()
     changed = False
-    for (n,m) in itertools.product( range(grid.x_max), range(grid.y_max)):
-        adjacent = [orig_grid.get(xx,yy) for (xx,yy) in  visible_seats[(n,m)]]
+    for (n,m) in grid.iterate_grid():
+        neighbour_seats = [orig_grid.get(xx,yy) for (xx,yy) in  grid.visible[(n,m)]]
+        num_occupied = neighbour_seats.count("#")
         if orig_grid.get(n,m) == "L":
-            if adjacent.count("#") == 0:
+            if num_occupied == 0:
                 grid.set(n,m, "#")
                 changed = True
         if orig_grid.get(n,m) == "#":
-            if adjacent.count("#") >=5:
+            if num_occupied >=occupancy_limit:
                 grid.set(n,m, "L")
                 changed = True
-    print("calculation done")
+    print(".", end='')
     return changed, grid
 
-def part_2(g):
-    visible_seats = cache_visible_seats(g)
+def full_solve_grid(g, *args):
     last_changed = True
     while last_changed:
-        last_changed, g = solve_grid_part_2(g, visible_seats)
-    print(g.count_char("#"))
+        last_changed, g = iterate_solve_grid(g, *args)
+    return g
 
+def part_2(g):
+    solved_g = full_solve_grid(g, np.inf, 5)
+    print("\npart 2:", solved_g.count_char("#"))
+
+def part_1(g):
+    solved_g = full_solve_grid(g, 1, 4)
+    print("\npart 1:", solved_g.count_char("#"))
+
+
+with open("input11.txt") as f:
+    lines =[line.strip() for line in f.readlines() if line.strip()]
+
+g = Grid(lines)
+#g.show()
+part_1(g.get_copy())
 part_2(g)
