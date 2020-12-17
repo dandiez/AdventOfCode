546748223f1e19f0dbbfa664b992b3a23b9e82b6
dandiez <47832466+dandiez@users.noreply.github.com>
2020-12-17 11:10:32 +0100

    clean up

--- day_17/solution.py
+++ day_17/solution.py
@@ -14,74 +14,59 @@ from unittest import TestCase
 def read_input(filename="input.txt"):
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
+    return lines
 
+
+@functools.lru_cache(None)
+def neighbours(p, dim):
+    n = set()
+    zero = (0,) * dim
+    for dp in itertools.product((-1, 0, 1), repeat=dim):
+        if dp != zero:
+            n.add(tuple(map(sum, zip(p, dp))))
+    return n
+
+
+def part_1_and_2(lines, dim, num_cycles=6):
     x_ini = len(lines[0])
     y_ini = len(lines)
-    z_ini = 0
-    w_ini = 0
-
     g = set()
     for x, line in enumerate(lines):
         for y, val in enumerate(line):
             if val == "#":
-                g.add((x, y, z_ini, w_ini))
-    inp = g
-    print(g)
+                g.add((x, y) + (0,) * (dim - 2))
 
-    return inp
-
-
-@functools.lru_cache(None)
-def neighbours(x, y, z, w):
-    n = []
-    for dx, dy, dz, dw in itertools.product((-1, 0, 1), repeat=4):
-        if (dx, dy, dz, dw) != (0, 0, 0, 0):
-            n.append((x + dx, y + dy, z + dz, w + dw))
-    return n
-
-
-def part_1(inp):
-    g = inp
-
-    def run_sim(g):
+    def run_sim(g, dim):
         g_new = set()
         grids_to_check = set()
-        for x, y, z, w in g:
-            grids_to_check = grids_to_check.union(set(neighbours(x, y, z, w)))
-        for x, y, z, w in grids_to_check:
-            active_n = 0
-            for n in neighbours(x, y, z, w):
-                if n in g:
-                    active_n += 1
-            if (x, y, z, w) in g and active_n in (2, 3):
-                g_new.add((x, y, z, w))
-            elif (x, y, z, w) not in g and active_n == 3:
-                g_new.add((x, y, z, w))
+        for p in g:
+            grids_to_check = grids_to_check.union((neighbours(p, dim)))
+        for p in grids_to_check:
+            active_n = len(g.intersection(neighbours(p, dim)))
+            if p in g and active_n in (2, 3):
+                g_new.add(p)
+            elif p not in g and active_n == 3:
+                g_new.add(p)
         return g_new
 
-    for i in range(6):
-        g = run_sim(g)
-        print(".")
-
+    for i in range(num_cycles):
+        g = run_sim(g, dim)
+        print(".", end="")
+    print("")
     p_1 = len(g)
     return p_1
-
-
-def part_2(inp):
-    p_2 = None
-    return p_2
 
 
 def main(input_file):
     """Solve puzzle and connect part 1 with part 2 if needed."""
     # part 1
     inp = read_input(input_file)
-    p1 = part_1(inp)
+    p1 = part_1_and_2(inp, 3)
     print(f"Solution to part 1: {p1}")
 
     # part 2
     inp = read_input(input_file)
-    p2 = part_2(inp)
+    p2 = part_1_and_2(inp, 4)
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
@@ -89,15 +74,9 @@ def main(input_file):
 def test_samples(self):
     input_file = "sample_1.txt"
     p1, p2 = main(input_file)
-    self.assertEqual(848, p1)
-    # self.assertEqual(112, p2)
-    print("***Tests 1 passed so far***")
-
-    # input_file = "sample_2.txt"
-    # p1, p2 = main(input_file)
-    # self.assertEqual( , p1)
-    # self.assertEqual( , p2)
-    print("***Tests 2 passed so far***")
+    self.assertEqual(112, p1)
+    self.assertEqual(848, p2)
+    print("***Tests passed so far***")
 
 
 if __name__ == "__main__":
