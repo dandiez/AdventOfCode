a798998262c7e3262566aa0f96005363fa5eb921
dandiez <47832466+dandiez@users.noreply.github.com>
2020-12-20 14:50:47 +0100

    clean up

--- day_20/solution.py
+++ day_20/solution.py
@@ -20,8 +20,6 @@ def read_input(filename="input.txt"):
         head, body = t.split(":\n")
         _, num = head.split("Tile ")
         tiles[num] = body
-        # print(num)
-        # print(body)
     inp = tiles
     return inp
 
@@ -35,39 +33,39 @@ def c_to_num(c):
         raise ValueError
 
 
+def get_rot_and_flip_variations(img):
+    return [np.rot90(img, k=n) for n in range(4)] + [
+        np.rot90(np.fliplr(img), k=n) for n in range(4)
+    ]
+
+
 class Tile:
     def __init__(self, num, body):
         self.num = num
         self._raw = body
         self.b = np.array([[c_to_num(c) for c in s] for s in body], int)
-        self.rotations = [np.rot90(self.b, k=n) for n in range(4)] + [
-            np.rot90(np.fliplr(self.b), k=n) for n in range(4)
-        ]
+        self.rotations = get_rot_and_flip_variations(self.b)
         assert len(self.rotations) == 8
-        self.position = 0
-        self.location = None
-
-    def locate(self, location):
-        self.location = location
-
-
-def validate(grid, tiles_dict):
-    pass
 
 
 def get_next_empty(grid, size):
+    """Grid will be filled left to right and top to bottom to simplify neighbour checks."""
     for r in range(size):
         for c in range(size):
             if grid[r, c] is None:
-                # print("Next empty is", (r, c))
-                return (r, c)
+                return r, c
 
 
 def get_rotated_tile(pos, tiles_dict):
+    """Return the variation given by the pos tuple.
+
+    pos: (tile_id, rotation_id)
+    """
     return tiles_dict[pos[0]].rotations[pos[1]]
 
 
 def lr_fit(left, right, tiles_dict):
+    """Check whether both tiles fit next to one another."""
     left_rc = get_rotated_tile(left, tiles_dict)[:, -1]
     right_lc = get_rotated_tile(right, tiles_dict)[:, 0]
     if all(left_rc == right_lc):
@@ -76,6 +74,7 @@ def lr_fit(left, right, tiles_dict):
 
 
 def tb_fit(top, bottom, tiles_dict):
+    """Check whether both tiles fit on top of each other."""
     top_br = get_rotated_tile(top, tiles_dict)[-1, :]
     bottom_tr = get_rotated_tile(bottom, tiles_dict)[0, :]
     if all(top_br == bottom_tr):
@@ -83,10 +82,14 @@ def tb_fit(top, bottom, tiles_dict):
     return False
 
 
-def get_candidates(grid, tiles_left, location, size, tiles_dict):
+def get_candidates(grid, tiles_left, location, tiles_dict):
+    """Return a list with all candidates that fit in the specified location.
+
+    Because we fill the grid from left to right and top to bottom, we only
+    need to check whether the tiles fit with the left and top neighbours (unless
+    they are on the first row or column."""
     # print(f"looking for candidates at location {location}")
     all_positions_left = itertools.product(tiles_left, range(8))
-
     candidates = []
     r, c = location
     if r != 0:
@@ -105,63 +108,33 @@ def get_candidates(grid, tiles_left, location, size, tiles_dict):
             if not lr_fit(left_n, pos, tiles_dict):
                 continue
         candidates.append(pos)
-
-    # print("Candidates found: ", candidates)
     return candidates
 
 
-def fits(one, another):
-    if one is None:
-        return True
-    return all(one == another)
-
-
 def solve(grid, tiles_left, size, tiles_dict):
-    # print("\nsolving with grid", len([v for k, v in grid.items() if v is not None]))
-    # print("Tiles left are", tiles_left)
+    """Run an iteration to solve the grid.
+
+    As long as there are tiles left to place, it tries each of them in any
+    possible position (rotation and flip) until the remaining tiles in the new
+    grid can also be solved."""
     if not tiles_left:
         return True
     location = get_next_empty(grid, size)
-    potential_positions = get_candidates(grid, tiles_left, location, size, tiles_dict)
+    potential_positions = get_candidates(grid, tiles_left, location, tiles_dict)
     while potential_positions:
-        # print(f"potential positions at location {location} are: {potential_positions}")
         t = potential_positions.pop()
         grid[location] = t
         new_tiles_left = [ti for ti in tiles_left if t[0] != ti]
-        # print("trying position ", t)
-        # print("new tiles left are ", new_tiles_left)
         result = solve(grid, new_tiles_left, size, tiles_dict)
         if result:
-            # print("solve returns True")
             return True
         else:
-            # print("solve returns False")
-            grid[location] = None
+            grid[location] = None  # remove the tile that did not fit from the grid
     return False
-
-    #
-    # all_tiles = tiles
-    # tiles_left = tiles
-    # tiles_dict = {t.num: t for t in tiles}
-    # tile_ids = [t.num for t in tiles]
-    # grid = np.zeros((s, s))
-    #
-    # tipos = list(itertools.product(tile_ids, range(4)))
-    # assert len(tipos) == len(tile_ids) * 4
-    #
-    # coord = itertools.product(range(s), repeat=2)
-    #
-    # print(tipos)
-    # print(coord)
-
-    # while tiles_left:
-    #     t = tiles_left.pop()
-    #     location = place_in_next_empty(grid, t.num)
-    #     t.location = location
-    #     if not validate(grid, tiles_dict):
 
 
 def create_image(grid, side_length, tiles_dict):
+    """Part 2 image from a given grid."""
     blocks = []
     for r in range(side_length):
         r_blocks = []
@@ -169,22 +142,20 @@ def create_image(grid, side_length, tiles_dict):
             pos = grid[(r, c)]
             t = get_rotated_tile(pos, tiles_dict)
             t_strip = t[1:-1, 1:-1]
-            # print(t_strip)
             r_blocks.append(t_strip)
-
         blocks.append(np.concatenate(r_blocks, axis=1))
-        # print(blocks)
     return np.concatenate(blocks, axis=0)
 
 
 def count_sea(variation, sea_monster):
+    """Find monsters and return empty sea water count."""
     monster_shape = sea_monster.shape
     variation_shape = variation.shape
     monster_space = np.sum(sea_monster)
     monsters_found = 0
     for r in range(variation_shape[0] - monster_shape[0]):
         for c in range(variation_shape[1] - monster_shape[1]):
-            search_space = variation[r : r + monster_shape[0], c : c + monster_shape[1]]
+            search_space = variation[r: r + monster_shape[0], c: c + monster_shape[1]]
             assert search_space.shape == monster_shape
             if np.sum(np.multiply(search_space, sea_monster)) == monster_space:
                 print("found a monster! coords:", r, c)
@@ -200,8 +171,6 @@ def main(input_file):
     for t, b in inp.items():
         body = [s.strip() for s in b.splitlines() if s.strip()]
         tiles.append(Tile(t, body))
-    # for t in tiles:
-    #    print(t.b)
 
     side_length = int(round(math.sqrt(len(tiles))))
     print(side_length)
@@ -211,12 +180,13 @@ def main(input_file):
     solution = solve(grid, tiles_left, side_length, tiles_dict)
     print(grid)
     p1 = (
-        int(grid[0, 0][0])
-        * int(grid[0, side_length - 1][0])
-        * int(grid[side_length - 1, 0][0])
-        * int(grid[side_length - 1, side_length - 1][0])
+            int(grid[0, 0][0])
+            * int(grid[0, side_length - 1][0])
+            * int(grid[side_length - 1, 0][0])
+            * int(grid[side_length - 1, side_length - 1][0])
     )
 
+    # part 2
     img = create_image(grid, side_length, tiles_dict)
     sea_monster = np.array(
         [
@@ -228,9 +198,7 @@ def main(input_file):
     print(img)
     print(img.shape)
     non_monsters = []
-    for variation in [np.rot90(img, k=n) for n in range(4)] + [
-        np.rot90(np.fliplr(img), k=n) for n in range(4)
-    ]:
+    for variation in get_rot_and_flip_variations(img):
         non_monsters.append(count_sea(variation, sea_monster))
 
     print("Non monsters", non_monsters)
