c06ab59fd81532d50dbedeadeb06affddcc255f6
dandiez <47832466+dandiez@users.noreply.github.com>
2020-12-25 14:36:04 +0100

    day 25 part 1

--- day_25/solution.py
+++ day_25/solution.py
@@ -1,29 +1,43 @@
-import re
-import networkx as nx
-from parse import *
-import copy
-from collections import defaultdict
-import itertools
-import numpy as np
-from math import cos, sin, pi
-import contextlib
 from unittest import TestCase
 
-
-def read_input(filename="input.txt"):
-    with open(filename) as f:
-        lines = [line.strip() for line in f.readlines() if line.strip()]
-    inp = lines  # parse here...
-    return inp
+BIG = 20201227
 
 
-def main(input_file):
+def loop_it_once(sn, num):
+    val = num * sn
+    return val % BIG
+
+
+MAX_IT = 1000000000
+
+def get_loop_size( pk, sn=7, num=1):
+    for loop_size in range(1, MAX_IT + 1):
+        num = loop_it_once(sn, num)
+        if num == pk:
+            print(f"found pk {pk} for loop size {loop_size}")
+            return loop_size
+        else:
+            pass
+            # print(f"pk {pk} does not match {num} for loop size {loop_size}")
+    raise Exception("loop size not found")
+
+def transform(sn, ls, num=1):
+    for n in range(ls):
+        num = loop_it_once(sn, num=num)
+    return num
+
+def main(input):
     """Solve puzzle and connect part 1 with part 2 if needed."""
     # part 1
-    inp = read_input(input_file)
+    cpk, dpk = input
 
+    cls = get_loop_size(cpk)
+    dls = get_loop_size(dpk)
+    enc_k_1 = transform(dpk, cls)
+    enc_k_2 = transform(cpk, dls)
+    assert enc_k_1 == enc_k_2
 
-    p1 = None
+    p1 = enc_k_1
     p2 = None
     print(f"Solution to part 1: {p1}")
     print(f"Solution to part 2: {p2}")
@@ -31,13 +45,14 @@ def main(input_file):
 
 
 def test_samples(self):
-    input_file = "sample_1.txt"
-    p1, p2 = main(input_file)
-    self.assertEqual( , p1)
+    input = (5764801, 17807724)  # card, door pk
+    p1, p2 = main(input)
+    self.assertEqual(14897079, p1)
     # self.assertEqual( , p2)
     print("***Tests passed so far***")
 
 
 if __name__ == "__main__":
     test_samples(TestCase())
-    main("full.txt")
+    input = (14788856, 19316454)
+    main(input)
