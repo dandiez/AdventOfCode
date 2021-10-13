b27fec85f7c6e4365c3eb435bc8a507a3bc017ce
dandiez <47832466+dandiez@users.noreply.github.com>
2021-10-13 21:29:54 +0200

    day 5 part 1

--- day_05/solution.py
+++ day_05/solution.py
@@ -1,8 +1,27 @@
-import contextlib
-import copy
-import itertools
-from dataclasses import dataclass
-from unittest import TestCase
+import operator
+from enum import Enum
+
+
+def main(input_file):
+    """Solve puzzle and connect part 1 with part 2 if needed."""
+    # part 1
+    inp = read_input(input_file)
+    p1 = part_1(inp)
+    print(f"Solution to part 1: {p1}")
+
+    # # part 2
+    # inp = read_input(input_file)
+    # p2 = part_2(inp)
+    # print(f"Solution to part 2: {p2}")
+    # return p1, p2
+
+
+def part_1(inp):
+    computer = IntcodeComputer(inp)
+    computer.set_input(1)
+    computer.execute()
+    p_1 = computer.outputs[-1]
+    return p_1
 
 
 def read_input(filename):
@@ -13,10 +32,20 @@ def read_input(filename):
     return inp
 
 
-@dataclass
+class Modes(Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+
 class Code:
-    op_code: int
-    modes: int
+
+    def __init__(self, code_raw: int):
+        self.op_code = code_raw % 100
+        modes_str = str(code_raw // 100)[::-1]
+        self.modes = {n: Modes(int(mode_str)) for n, mode_str in enumerate(modes_str)}
+
+    def get_n_mode(self, n: int):
+        return self.modes.get(n, Modes.POSITION)
 
 
 class IntcodeComputer:
@@ -24,8 +53,13 @@ class IntcodeComputer:
     def __init__(self, memory):
         self.memory = memory
         self.position = 0
-        self.max_cycles = 10000
+        self.max_cycles = 1000000
         self.stop = False
+        self.input = None
+        self.outputs = list()
+
+    def set_input(self, input_to_set):
+        self.input = input_to_set
 
     def read_and_skip(self):
         value = self.memory[self.position]
@@ -36,88 +70,55 @@ class IntcodeComputer:
         num_cycles = 0
         while num_cycles <= self.max_cycles and not self.stop:
             self.process_instruction()
+            num_cycles += 1
+        if num_cycles >= self.max_cycles:
+            raise RecursionError("Reached max cycles.")
 
     def process_instruction(self):
         code_raw = self.read_and_skip()
-        code = self.parse_code(code_raw)
+        code = Code(code_raw)
         if code.op_code == 1:
             self.process_sum(code)
         elif code.op_code == 2:
-            self.process_mult(code)
+            self.process_mul(code)
+        elif code.op_code == 3:
+            self.process_save_input(code)
+        elif code.op_code == 4:
+            self.process_output(code)
         elif code.op_code == 99:
             self.stop = True
+        else:
+            raise ValueError(f"Unknown code {code.op_code}")
+
+    def process_save_input(self, code):
+        self.memory[self.read_and_skip()] = self.input
+
+    def process_output(self, code):
+        value = self.read_n_parameter_values(code, 1)[0]
+        print(value)
+        self.outputs.append(value)
+
+    def process_aritm(self, code, f: callable):
+        params = self.read_n_parameter_values(code, 2)
+        output_address = self.read_and_skip()
+        self.memory[output_address] = f(*params)
 
     def process_sum(self, code):
-        p1 = self.read_and_skip()
-        p2 = self.read_and_skip()
-        out = self.read_and_skip()
-        self.memory[out] = self.memory[p1] + self.memory[p2]
+        self.process_aritm(code, operator.add)
 
-    def process_mult(self, code):
-        p1 = self.read_and_skip()
-        p2 = self.read_and_skip()
-        out = self.read_and_skip()
-        self.memory[out] = self.memory[p1] * self.memory[p2]
+    def process_mul(self, code):
+        self.process_aritm(code, operator.mul)
 
-    @staticmethod
-    def parse_code(code_raw):
-        op_code = code_raw % 100
-        modes = code_raw // 100
-        code = Code(op_code=op_code, modes=modes)
-        return code
-
-
-def part_1(inp, noun=12, verb=2):
-    if noun is not None:
-        inp[1] = noun
-    if verb is not None:
-        inp[2] = verb
-    p = 0
-    computer = IntcodeComputer(inp)
-    computer.execute()
-
-    p_1 = inp[0]
-    return p_1
-
-
-def part_2(inp):
-    for (n, v) in itertools.product(range(100), repeat=2):
-        with contextlib.suppress(Exception):
-            p_1 = part_1(copy.deepcopy(inp), noun=n, verb=v)
-        if p_1 == 19690720:
-            return 100 * n + v
-    p_2 = None
-    return p_2
-
-
-def main(input_file, **kwargs):
-    """Solve puzzle and connect part 1 with part 2 if needed."""
-    # part 1
-    inp = read_input(input_file)
-    p1 = part_1(inp, **kwargs)
-    print(f"Solution to part 1: {p1}")
-
-    # part 2
-    inp = read_input(input_file)
-    p2 = part_2(inp)
-    print(f"Solution to part 2: {p2}")
-    return p1, p2
-
-
-def test_sample_1(self):
-    input_file = "sample_1.txt"
-    p1, p2 = main(input_file, noun=None, verb=None)
-    self.assertEqual(30, p1)
-    self.assertEqual(3500, part_1([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], noun=None, verb=None))
-    self.assertEqual(2, part_1([1, 0, 0, 0, 99], noun=None, verb=None))
-    self.assertEqual(2, part_1([2, 3, 0, 3, 99], noun=None, verb=None))
-    self.assertEqual(2, part_1([2, 4, 4, 5, 99, 0], noun=None, verb=None))
-    self.assertEqual(30, part_1([1, 1, 1, 4, 99, 5, 6, 0, 99], noun=None, verb=None))
-    # self.assertEqual(1202, part_1([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], noun=12, verb=2))
-    # self.assertEqual(  , p2)
-    print("***Tests 1 passed so far***")
+    def read_n_parameter_values(self, code: Code, number_of_parameter_values_to_get: int):
+        values = []
+        for n in range(number_of_parameter_values_to_get):
+            mode_n = code.get_n_mode(n)
+            if mode_n is Modes.POSITION:
+                values.append(self.memory[self.read_and_skip()])
+            else:
+                values.append(self.read_and_skip())
+        return values
 
 
 if __name__ == "__main__":
-    test_sample_1(TestCase())
     main("input")
