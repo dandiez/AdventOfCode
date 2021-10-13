d062ac7391561e1b9b0b3040493d532bbd7bf5fb
dandiez <47832466+dandiez@users.noreply.github.com>
2021-10-13 19:59:40 +0200

    template

---
+++ day_xx/solution.py
@@ -0,0 +1,45 @@
+
+from unittest import TestCase
+
+
+def read_input(filename="input.txt"):
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+    inp = [int(val) for val in lines]  # parse here...
+    return inp
+
+
+def part_1(inp):
+    pass
+
+
+def part_2(inp):
+    pass
+
+
+def main(input_file):
+    """Solve puzzle and connect part 1 with part 2 if needed."""
+    # part 1
+    inp = read_input(input_file)
+    p1 = part_1(inp)
+    print(p1)
+
+    # part 2
+    inp = read_input(input_file)
+    p2 = part_2(inp)
+    print(p2)
+    return p1, p2
+
+
+def test_sample_1(self):
+    pass
+
+
+def test_sample_2(self):
+    pass
+
+
+if __name__ == "__main__":
+    test_sample_1(TestCase())
+    test_sample_2(TestCase())
+    main("input")
