acc2acd33926ed11b28ea3c9517640830d1cbe0e
dandiez <47832466+dandiez@users.noreply.github.com>
2021-10-17 19:55:36 +0200

    day 23 part 2

--- day_23/solution.py
+++ day_23/solution.py
@@ -8,6 +8,7 @@ import logging
 
 logger = logging.getLogger("day_23")
 
+
 def main(input_file):
     """Solve puzzle and connect part 1 with part 2 if needed."""
     # part 1
@@ -18,6 +19,7 @@ def main(input_file):
     # part 2
     inp = read_input(input_file)
     p2 = part_2(inp)
+    print(f"Solution to part 1: {p1}")
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
@@ -37,7 +39,7 @@ class Package:
 
 
 class Network:
-    def __init__(self, inp, num_computers = 50):
+    def __init__(self, inp, num_computers=50):
         self.computers: Dict[int, IntcodeComputer] = {
             n: IntcodeComputer(inp) for n in range(num_computers)
         }
@@ -46,6 +48,8 @@ class Network:
         self.package_sender_task = None
         self.full_packages_queue: asyncio.Queue[Package] = asyncio.Queue()
         self.part_1_answer = asyncio.Queue()
+        self.part_2_answer = asyncio.Queue()
+        self.last_package_255 = None
 
     def configure(self):
         for n, computer in self.computers.items():
@@ -58,45 +62,69 @@ class Network:
         self.package_sender_task = asyncio.create_task(self.package_sender())
 
     async def package_sender(self):
-        print("Started package sender")
+        logger.debug("Started package sender")
         while True:
             # need small sleep or otherwise it deadlocks
-            await asyncio.sleep(0.001)
+            await asyncio.sleep(0.00001)
             await self.forward_ready_packages_to_destination()
             await self.feed_computers_with_empty_input_queues()
 
     async def forward_ready_packages_to_destination(self):
         while not self.full_packages_queue.empty():
             package: Package = await self.full_packages_queue.get()
-            print(f"Received package {package}")
+            logger.debug(f"Received package {package}")
             if package.destination == 255:
                 await self.part_1_answer.put(package.y)
-            try:
-                await self.computers[package.destination].input_queue.put(package.x)
-                await self.computers[package.destination].input_queue.put(package.y)
-            except KeyError:
-                logger.error(f"Unknown destination {package.destination}")
+                self.last_package_255 = package
+            else:
+                try:
+                    await self.computers[package.destination].input_queue.put(package.x)
+                    await self.computers[package.destination].input_queue.put(package.y)
+                except KeyError:
+                    logger.error(f"Unknown destination {package.destination}")
+
+    async def nat(self):
+        sent_y_values = set()
+        while True:
+            await asyncio.sleep(0.0001)
+            if self.full_packages_queue.empty():
+                # wait a bit to see if network is really idle...
+                await asyncio.sleep(0.1)
+                if self.full_packages_queue.empty():
+                    package_to_send: Package = self.last_package_255
+                    package_to_send.destination = 0
+                    if package_to_send is None:
+                        continue
+                    if package_to_send.y in sent_y_values:
+                        await self.part_2_answer.put(package_to_send.y)
+                    else:
+                        sent_y_values.add(package_to_send.y)
+                    logger.warning(f"Network is idle. Sending {package_to_send}.")
+                    await self.full_packages_queue.put(package_to_send)
 
     async def feed_computers_with_empty_input_queues(self):
         for n, computer in self.computers.items():
             if computer.input_queue.empty():
                 await computer.input_queue.put(-1)
 
-    async def start_network_loop(self):
+    async def start_network_loop(self, is_part_1=False):
         self.configure()
         self.computer_tasks = {
             n: asyncio.create_task(computer.execute())
             for n, computer in self.computers.items()
         }
-        return await self.part_1_answer.get()
+        self.nat_task = asyncio.create_task(self.nat())
+        if is_part_1:
+            return await self.part_1_answer.get()
+        return await self.part_2_answer.get()
 
     async def build_packages_helper(
         self,
         queue_partial_packages: asyncio.Queue[int],
         queue_full_packages: asyncio.Queue[Package],
-        computer_id
+        computer_id,
     ):
-        print(f"Starting package helper for computer {computer_id}")
+        logger.debug(f"Starting package helper for computer {computer_id}")
         while True:
             destination_id = await queue_partial_packages.get()
             x = await queue_partial_packages.get()
@@ -104,16 +132,18 @@ class Network:
             package = Package(x=x, y=y, destination=destination_id)
             await queue_full_packages.put(package)
 
-async def main_loop(inp):
+
+async def main_loop(inp, is_part_1=False):
     network = Network(inp)
-    return await network.start_network_loop()
+    return await network.start_network_loop(is_part_1=is_part_1)
+
 
 def part_1(inp):
-    return asyncio.run(main_loop(inp))
+    return asyncio.run(main_loop(inp, is_part_1=True))
 
 
 def part_2(inp):
-    pass
+    return asyncio.run(main_loop(inp))
 
 
 def test_sample_1(self):
