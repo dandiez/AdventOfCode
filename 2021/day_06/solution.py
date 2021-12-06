3e17cd2452632b2b55b7d5d1685df91cfe8be13a
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-06 08:44:29 +0100

    day 06 part 1

---
+++ 2021/day_06/solution.py
@@ -0,0 +1,52 @@
+from unittest import TestCase
+
+
+def read_input(filename="input"):
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+    inp = [int(val) for val in lines[0].split(',')]  # parse here...
+    return inp
+
+def part_1(inp):
+    for n in range(80):
+        inp=simulate_one(inp)
+    return len(inp)
+def simulate_one(inp):
+    new_inp = []
+    for f in inp:
+        if f == 0:
+            new_inp.append(6)
+            new_inp.append(8)
+        else:#
+            new_inp.append(f-1)
+    return new_inp
+def part_2(inp):
+    pass
+
+def main(input_file):
+    """Solve puzzle and connect part 1 with part 2 if needed."""
+    # part 1
+    inp = read_input(input_file)
+    p1 = part_1(inp)
+    print(f"Solution to part 1: {p1}")
+
+    # part 2
+    inp = read_input(input_file)
+    p2 = part_2(inp)
+    print(f"Solution to part 2: {p2}")
+    return p1, p2
+
+def test_sample_1(self):
+    inp = read_input("sample_1")
+    self.assertEqual(5934, part_1(inp))
+    pass
+
+def test_sample_2(self):
+    pass
+
+if __name__ == "__main__":
+    print('*** solving tests ***')
+    test_sample_1(TestCase())
+    test_sample_2(TestCase())
+    print('*** solving main ***')
+    main("input")
