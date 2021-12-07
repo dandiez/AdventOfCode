c7e856fe5f9d5ddb6630035c1291cb943a1c77b9
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-07 09:11:06 +0100

    day 07 part 2

--- 2021/day_07/solution.py
+++ 2021/day_07/solution.py
@@ -9,28 +9,37 @@ def read_input(filename="input"):
     inp = [int(val) for val in lines[0].split(',')]  # parse here...
     return inp
 
+
 def part_1(inp):
     arr = np.array(inp)
-    return find_minimum(arr)
+    return find_minimum(arr, calc_fuel)
 
 
-def find_minimum(arr):
+def find_minimum(arr, func):
     _min = min(arr)
     _max = max(arr)
     _min_fuel_found = 999999999999
     for n in range(_min, _max + 1):
-        fuel = calc_fuel(arr, n)
+        fuel = func(arr, n)
         if fuel < _min_fuel_found:
             _min_fuel_found = fuel
     return _min_fuel_found
+
 
 def calc_fuel(arr, pos):
     return np.abs(arr - pos).sum()
 
 
-
 def part_2(inp):
-    pass
+    arr = np.array(inp)
+    return find_minimum(arr, calc_fuel_2)
+
+
+def calc_fuel_2(arr, pos):
+    deltas = np.abs(arr - pos)
+    fuel_cost = (deltas / 2) * (deltas + 1)
+    return fuel_cost.sum()
+
 
 def main(input_file):
     """Solve puzzle and connect part 1 with part 2 if needed."""
@@ -45,13 +54,16 @@ def main(input_file):
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
+
 def test_sample_1(self):
     inp = read_input("sample_1")
     self.assertEqual(37, part_1(inp))
+    self.assertEqual(168, part_2(inp))
 
 
 def test_sample_2(self):
     pass
+
 
 if __name__ == "__main__":
     print('*** solving tests ***')
