028014de889b41dd4f8e17770daba1a57268dc9e
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-08 10:04:39 +0100

    day 08 part 2

--- 2021/day_08/solution.py
+++ 2021/day_08/solution.py
@@ -1,3 +1,4 @@
+import itertools
 from unittest import TestCase
 
 
@@ -7,11 +8,13 @@ def read_input(filename="input"):
     inp = [parse_line(line) for line in lines]  # parse here...
     return inp
 
+
 def parse_line(line):
     left, right = line.split('|')
     ten_digits = [val.strip() for val in left.split()]
     four_digits = [val.strip() for val in right.split()]
     return ten_digits, four_digits
+
 
 def part_1(inp):
     print(inp)
@@ -22,8 +25,55 @@ def part_1(inp):
                 count += 1
     return count
 
+
 def part_2(inp):
-    pass
+    return sum(get_number(line) for line in inp)
+
+
+segment_map = {
+    (0, 1, 2, 3, 5, 6): 0,
+    (3, 6): 1,
+    (0, 1, 3, 4, 5): 2,
+    (0, 1, 3, 4, 6): 3,
+    (2, 3, 4, 6): 4,
+    (0, 1, 2, 4, 6): 5,
+    (0, 1, 2, 4, 5, 6): 6,
+    (1, 3, 6): 7,
+    (0, 1, 2, 3, 4, 5, 6): 8,
+    (0, 1, 2, 3, 4, 6): 9,
+}
+
+
+def get_number(line):
+    ten, four = line
+    for perm in itertools.permutations('abcdefg'):
+        key = {letter: segment for segment, letter in enumerate(perm)}
+        try:
+            decode_all(ten, key)
+        except ValueError:
+            continue
+        else:
+            return decode_four(four, key)
+
+
+def decode(str_num, key ):
+    segments = []
+    for digit in str_num:
+        segments.append(key[digit])
+    segs = tuple(sorted(segments))
+    if tuple(sorted(segments)) in segment_map:
+        return segment_map[segs]
+    raise ValueError('not possible')
+
+def decode_four(four, key):
+    decoded = [decode(str_num, key) for str_num in four]
+    return decoded[0]*1000+decoded[1]*100+decoded[2]*10+decoded[3]
+
+
+def decode_all(ten, key):
+    for num in ten:
+        decode(num, key)
+
 
 def main(input_file):
     """Solve puzzle and connect part 1 with part 2 if needed."""
@@ -38,13 +88,16 @@ def main(input_file):
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
+
 def test_sample_1(self):
     inp = read_input("sample_1")
     self.assertEqual(26, part_1(inp))
+    self.assertEqual(61229, part_2(inp))
 
 
 def test_sample_2(self):
     pass
+
 
 if __name__ == "__main__":
     print('*** solving tests ***')
