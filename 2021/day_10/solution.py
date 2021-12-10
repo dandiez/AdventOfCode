d0f681c27bb9f1ea0a4e3f1a430d4b983b9954af
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-10 20:06:32 +0100

    day 19 part 2

--- 2021/day_10/solution.py
+++ 2021/day_10/solution.py
@@ -9,8 +9,8 @@ def read_input(filename="input"):
 
 
 def part_1(inp):
-    return sum(get_parse_error_points(line) for line in inp)
-
+    # return sum(get_parse_error_points(line) for line in inp)
+    ...
 
 char_pairs = {"(": ")", "[": "]", "{": "}", "<": ">"}
 
@@ -30,15 +30,26 @@ def get_parse_error_points(line):
         else:
             expected_start = stack.pop()
             if char != char_pairs[expected_start]:
-                return char_points[char]
-    return 0
+                return (char_points[char], None)
+    endings = [char_pairs[char] for char in reversed(stack)]
+    return (0, endings)
 
 def is_valid_start(char):
     return char in char_pairs.keys()
 
 
 def part_2(inp):
-    pass
+    scores = []
+    for line in inp:
+        points, ending = get_parse_error_points(line)
+        if points == 0:
+            scores.append(score_ending(ending))
+    return get_middle_score(scores)
+
+
+def get_middle_score(scores):
+    scores.sort()
+    return scores[len(scores) // 2 ]
 
 
 def main(input_file):
@@ -54,11 +65,25 @@ def main(input_file):
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
+ending_scores = {
+')': 1 ,
+']': 2 ,
+'}': 3 ,
+'>': 4,
+}
+
+def score_ending(ending):
+    total = 0
+    for char in ending:
+        total = 5* total
+        total += ending_scores[char]
+    return total
 
 def test_sample_1(self):
     inp = read_input("sample_1")
-    self.assertEqual(26397, part_1(inp))
-
+    #self.assertEqual(26397, part_1(inp))
+    self.assertEqual(288957, score_ending(list('}}]])})]')))
+    self.assertEqual(4, get_middle_score([4, 5, 2]))
 
 def test_sample_2(self):
     pass
