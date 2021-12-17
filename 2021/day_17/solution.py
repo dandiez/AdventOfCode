709f3e10fcedea0f5c52bccd57ce2a17ec482e4a
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-17 10:58:32 +0100

    day 17 part 2 better impossible to reach for quicker answer

--- 2021/day_17/solution.py
+++ 2021/day_17/solution.py
@@ -1,15 +1,22 @@
 from unittest import TestCase
 from parse import parse
 
+
 def read_input(filename="input"):
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
-    xmin, xmax, ymin, ymax = parse('target area: x={:d}..{:d}, y={:d}..{:d}', lines[0])
+    xmin, xmax, ymin, ymax = parse("target area: x={:d}..{:d}, y={:d}..{:d}", lines[0])
     return xmin, xmax, ymin, ymax
+
 
 def part_1(inp):
     xmin, xmax, ymin, ymax = inp
-    return sum(-(ymin+1)-n for n in range(-ymin))
+    # when crossing zero: vy = -(vy_initial + 1)
+    # highest vy downwards to just reach ymin: vy = ymin
+    #  ==> vy_initial = -(ymin + 1)
+    # This assumes x has reached vx=0 and is at target x range.
+    return sum(-(ymin + 1) - n for n in range(-ymin))
+
 
 class Simulation:
     def __init__(self, xmin, xmax, ymin, ymax, vx, vy):
@@ -21,13 +28,13 @@ class Simulation:
         self.vy = vy
         self.x = 0
         self.y = 0
-        self.num_solutions = 0
+        self.target_hit = False
 
     def simulate(self):
         while True:
             self.simulate_step()
             if self.in_target():
-                self.num_solutions+=1
+                self.target_hit = True
                 break
             if self.impossible_to_reach():
                 break
@@ -47,38 +54,24 @@ class Simulation:
     def impossible_to_reach(self):
         if self.y < self.ymin:
             return True
-        if self.vx <= 0:
+        if self.x > self.xmax:
+            return True
+        if self.vx == 0:
             if not (self.xmin <= self.x <= self.xmax):
-                return False
-
+                return True
+        return False
 
 
 def part_2(inp):
     xmin, xmax, ymin, ymax = inp
     num_solutions = 0
-    for vx in range(1, xmax+1):
-        for vy in range(ymin-1, -ymin+2):
+    for vx in range(1, xmax + 1):
+        for vy in range(ymin - 1, -ymin + 2):
             s = Simulation(xmin, xmax, ymin, ymax, vx, vy)
             s.simulate()
-            num_solutions += s.num_solutions
+            if s.target_hit:
+                num_solutions += 1
     return num_solutions
-
-
-
-def simulate_x(vx, xmin, xmax):
-    target_hits = set()
-    step = 0
-    x = 0
-    while True:
-        step += 1
-        x += vx
-        vx -= 1
-        if xmin <= x <= xmax:
-            target_hits.add(step)
-            if vx == 0:
-                return target_hits, True
-        if vx <= 0:
-            return target_hits, False
 
 
 def main(input_file):
@@ -94,17 +87,20 @@ def main(input_file):
     print(f"Solution to part 2: {p2}")
     return p1, p2
 
+
 def test_sample_1(self):
     inp = read_input("sample_1")
     self.assertEqual(45, part_1(inp))
     self.assertEqual(112, part_2(inp))
 
+
 def test_sample_2(self):
     pass
 
+
 if __name__ == "__main__":
-    print('*** solving tests ***')
+    print("*** solving tests ***")
     test_sample_1(TestCase())
     test_sample_2(TestCase())
-    print('*** solving main ***')
+    print("*** solving main ***")
     main("input")
