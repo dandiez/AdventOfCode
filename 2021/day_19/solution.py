195967fdf8b991c6af0ada606f55620d515fb904
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-19 13:22:27 +0100

    day 19 part 1 and 2

--- 2021/day_19/solution.py
+++ 2021/day_19/solution.py
@@ -6,7 +6,18 @@ import networkx as nx
 import numpy as np
 from numpy.linalg import linalg
 from scipy.spatial.distance import cityblock
-from scipy.spatial.transform import Rotation
+from scipy.spatial.transform import Rotation as R
+
+
+def main(input_file):
+    """Solve puzzle and connect part 1 with part 2 if needed."""
+    inp = read_input(input_file)
+    transformations = get_all_transformations(inp)
+    p1 = part_1(inp, transformations)
+    print(f"Solution to part 1: {p1}")
+    p2 = part_2(transformations)
+    print(f"Solution to part 2: {p2}")
+    return p1, p2
 
 
 def read_input(filename="input"):
@@ -26,47 +37,15 @@ def parse_inp(inp):
     return raw
 
 
-def get_all_rotations():
-    yield from [Rotation.from_euler("x", 90 * i, degrees=True) for i in range(4)]
-    yield from [Rotation.from_euler("zx", [90, 90 * i], degrees=True) for i in range(4)]
-    yield from [
-        Rotation.from_euler("zx", [180, 90 * i], degrees=True) for i in range(4)
-    ]
-    yield from [
-        Rotation.from_euler("zx", [-90, 90 * i], degrees=True) for i in range(4)
-    ]
-    yield from [Rotation.from_euler("yx", [90, 90 * i], degrees=True) for i in range(4)]
-    yield from [
-        Rotation.from_euler("yx", [-90, 90 * i], degrees=True) for i in range(4)
-    ]
+def get_all_transformations(inp):
+    """Find transf. matrices for all scanners. Absolute is scanner zero."""
+    rel_transf = find_rel_transformations(inp)
+    abs_transf = find_abs_transformations(rel_transf)
+    return abs_transf
 
 
-def part_1(inp):
-    overlap_transformations = trans_overlapping(inp)
-    transformations = trans_rel_to_0(overlap_transformations)
-    abs_point_coords = all_points_rel_to_0(inp, transformations)
-    max_d = find_largest_manhattan(transformations)
-    print(max_d)
-    return len(abs_point_coords)
-
-
-def find_largest_manhattan(transformations):
-    max_d = 0
-    for i, vi in transformations.items():
-        for j, vj in transformations.items():
-            if i == j:
-                continue
-            pi = vi[:, -1]
-            pj = vj[:, -1]
-            d = cityblock(pi, pj)
-            max_d = max(max_d, d)
-    return max_d
-
-
-def trans_overlapping(inp):
-    """Find the transformations across scanners overlapping.
-
-    Return dict of transformation matrices across overlapping scanners.
+def find_rel_transformations(inp):
+    """Return dict of transformation matrices across overlapping scanners.
     { (i, j): np.array() }
     """
     num_scanners = len(inp)
@@ -77,20 +56,6 @@ def trans_overlapping(inp):
             overlaps[(i, j)] = transformation
             overlaps[(j, i)] = as_int(linalg.inv(transformation))
     return overlaps
-
-
-def trans_rel_to_0(overlaps):
-    """Chain transformations to arrive at any target from zero."""
-    transformations = {}
-    G = nx.Graph()
-    G.add_edges_from(overlaps.keys())
-    paths = nx.shortest_path(G, source=0)
-    for target, path in paths.items():
-        trn = np.identity(4, int)
-        for n in range(len(path) - 1):
-            trn = np.matmul(trn, overlaps[(path[n], path[n + 1])])
-        transformations[target] = trn
-    return transformations
 
 
 def scanners_overlap(points_1, points_2):
@@ -109,66 +74,84 @@ def scanners_overlap(points_1, points_2):
             return transformation
 
 
-def find_best_shift(points_1, points_2):
-    """Given two clouds of points, find the shift that overlaps most points.
-    The shift is given as column vector.
-    """
-    p1s = [as_int(a) for a in points_1]
-    p2s = [as_int(a) for a in points_2]
-    differences = defaultdict(int)
-    for p1 in p1s:
-        for p2 in p2s:
-            differences[tuple(p1 - p2)] += 1
-    most_in_common = max(differences.values())
-    for k, v in differences.items():
-        if v == most_in_common:
-            return np.array([k], int).T, v
-
-
 def as_int(arr):
     return np.rint(arr).astype(int)
 
 
-def all_points_rel_to_0(inp, transformations):
-    """Use the transformations to express all points in abs. coordinates."""
+def get_all_rotations():
+    yield from [R.from_euler("x", 90 * i, degrees=True) for i in range(4)]
+    yield from [R.from_euler("zx", [90, 90 * i], degrees=True) for i in range(4)]
+    yield from [R.from_euler("zx", [180, 90 * i], degrees=True) for i in range(4)]
+    yield from [R.from_euler("zx", [-90, 90 * i], degrees=True) for i in range(4)]
+    yield from [R.from_euler("yx", [90, 90 * i], degrees=True) for i in range(4)]
+    yield from [R.from_euler("yx", [-90, 90 * i], degrees=True) for i in range(4)]
+
+
+def find_best_shift(points_1, points_2):
+    """Given two clouds of points, find the shift that overlaps most points.
+    The shift is given as column vector.
+    Also return the number of points overlapping for that shift.
+    """
+    p1s = [as_int(a) for a in points_1]
+    p2s = [as_int(a) for a in points_2]
+    shifts = defaultdict(int)
+    for p1 in p1s:
+        for p2 in p2s:
+            shifts[tuple(p1 - p2)] += 1
+    max_points_overlapping = max(shifts.values())
+    for shift, num_overlapping in shifts.items():
+        if num_overlapping == max_points_overlapping:
+            return np.array([shift], int).T, num_overlapping
+
+
+def find_abs_transformations(overlaps):
+    """Chain transformations to arrive at any target from the zero scanner."""
+    transformations = {}
+    G = nx.Graph()
+    G.add_edges_from(overlaps.keys())
+    paths = nx.shortest_path(G, source=0)
+    for target, path in paths.items():
+        trn = np.identity(4, int)
+        for n in range(len(path) - 1):
+            trn = np.matmul(trn, overlaps[(path[n], path[n + 1])])
+        transformations[target] = trn
+    return transformations
+
+
+def part_1(inp, transformations):
+    abs_point_coords = all_abs_points(inp, transformations)
+    return len(abs_point_coords)
+
+
+def all_abs_points(inp, transformations):
+    """Express all points in absolute coordinates."""
     abs_points = set()
     for csys, points in inp.items():
         trn = transformations[csys]
         point_cloud = np.vstack((np.array(points).T, np.ones(len(points))))
-        abs_csys_points = np.matmul(trn, point_cloud)
-        abs_csys_points = abs_csys_points[:-1].T.tolist()
-        abs_csys_points = set(tuple(p) for p in abs_csys_points)
-        abs_points.update(abs_csys_points)
+        point_cloud_abs = np.matmul(trn, point_cloud)
+        point_cloud_abs = point_cloud_abs[:-1].T.tolist()
+        point_cloud_abs = set(tuple(p) for p in point_cloud_abs)
+        abs_points.update(point_cloud_abs)
     return abs_points
 
 
-
-def main(input_file):
-    """Solve puzzle and connect part 1 with part 2 if needed."""
-    # part 1
-    inp = read_input(input_file)
-    p1 = part_1(inp)
-    print(f"Solution to part 1: {p1}")
-
-    # part 2
-    inp = read_input(input_file)
-    p2 = part_2(inp)
-    print(f"Solution to part 2: {p2}")
-    return p1, p2
+def part_2(transformations):
+    max_d = 0
+    for (i, vi), (j, vj) in combinations(transformations.items(), 2):
+        pi = vi[:, -1]
+        pj = vj[:, -1]
+        d = cityblock(pi, pj)
+        max_d = max(max_d, d)
+    return max_d
 
 
 def test_sample_1(self):
-    inp = read_input("sample_1")
-    self.assertEqual(79, part_1(inp))
-
-
-def test_sample_2(self):
-    pass
+    self.assertEqual((79, 3621), main("sample_1"))
 
 
 if __name__ == "__main__":
     print("*** solving tests ***")
     test_sample_1(TestCase())
-    test_sample_2(TestCase())
     print("*** solving main ***")
     main("input")
