abf1e998f7aba0a079a8e4d4bb65f224db551013
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-21 12:38:53 +0100

    part 1 and 2

--- 2021/day_21/solution.py
+++ 2021/day_21/solution.py
@@ -16,39 +16,48 @@ def cyclic_d100():
         yield n
 
 
-practice_die = cyclic_d100()
-
-
-def cyclic_3d100():
-    return sum(next(practice_die) for _ in range(3))
-
-
 class PracticeGame:
-    def __init__(self, pos_1, pos_2):
-        self.live_games = defaultdict(int)
-        self.live_games[(pos_1, pos_2, 0, 0, True, False)] += 1
-        self.win_count = defaultdict(int)
+    def __init__(self, p1_start, p2_start):
+        self.player_pos = [p1_start, p2_start]
+        self.player_points = [0, 0]
         self.roll_count = 0
+        self.die = cyclic_d100()
+        self.num_players = len(self.player_points)
+
+    def roll(self):
+        self.roll_count += 1
+        return next(self.die)
+
+    def roll_n(self, n):
+        roll_sum = 0
+        for _ in range(n):
+            roll_sum += self.roll()
+        return roll_sum
 
     def play(self):
-        game = list(self.live_games.keys())[0]
-        pos_1, pos_2, points_1, points_2, player_turn, is_won = game
-        while not is_won:
-            # print(self.live_games, self.win_count)
-            game, count = self.live_games.popitem()
-            new_games = get_new_game_states(game, practice=True)
-            self.roll_count += 3
-            for game in new_games:
-                pos_1, pos_2, points_1, points_2, player_turn, is_won = game
-                if is_won:
-                    self.win_count[player_turn] += count
-                else:
-                    self.live_games[game] += count
-        return min(points_1, points_2) * self.roll_count
+        last_score = 0
+        for p in cycle(range(self.num_players)):
+            if last_score >= 1000:
+                return self.player_points[p] * self.roll_count
+            roll = self.roll_n(3)
+            self.player_pos[p] = self.new_pos(self.player_pos[p], roll)
+            self.player_points[p] += self.player_pos[p]
+            last_score = self.player_points[p]
+
+    def new_pos(self, old_pos, roll):
+        new = old_pos + roll
+        return (new - 1) % 10 + 1
+
+
+def dirac_3d3():
+    for i in range(1, 4):
+        for j in range(1, 4):
+            for k in range(1, 4):
+                yield i + j + k
 
 
 @lru_cache(None)
-def play_turn(game_state, roll, win_target=21):
+def get_new_state(game_state, roll, win_target=21):
     pos_1, pos_2, points_1, points_2, player_turn, is_won = game_state
     if player_turn == True:
         pos_1 = (pos_1 + roll - 1) % 10 + 1
@@ -61,22 +70,12 @@ def play_turn(game_state, roll, win_target=21):
     return pos_1, pos_2, points_1, points_2, not player_turn, is_won
 
 
-def roll_outcomes():
-    for i in range(1, 4):
-        for j in range(1, 4):
-            for k in range(1, 4):
-                yield i + j + k
-
-
 @lru_cache(None)
-def get_new_game_states(game_state: tuple, practice=False):
-    if practice:
-        return (play_turn(game_state, cyclic_3d100(), win_target=1000),)
-    else:
-        new = []
-        for roll_outcome in roll_outcomes():
-            new.append(play_turn(game_state, roll_outcome))
-        return tuple(new)
+def get_new_game_states(game_state: tuple):
+    new = []
+    for roll_outcome in dirac_3d3():
+        new.append(get_new_state(game_state, roll_outcome))
+    return new
 
 
 class DiracGame:
@@ -87,7 +86,6 @@ class DiracGame:
 
     def play(self):
         while sum(self.live_games.values()):
-            # print(self.live_games, self.win_count)
             game, count = self.live_games.popitem()
             new_games = get_new_game_states(game)
             for game in new_games:
@@ -129,13 +127,8 @@ def test_sample_1(self):
     self.assertEqual(444356092776315, part_2(inp))
 
 
-def test_sample_2(self):
-    pass
-
-
 if __name__ == "__main__":
     print("*** solving tests ***")
     test_sample_1(TestCase())
-    test_sample_2(TestCase())
     print("*** solving main ***")
     main("input")
