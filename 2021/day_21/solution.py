3e33e11a2d40fa69bc9058700d974f8a5104cf8d
dandiez <47832466+dandiez@users.noreply.github.com>
2021-12-21 12:02:43 +0100

    format

--- 2021/day_21/solution.py
+++ 2021/day_21/solution.py
@@ -15,10 +15,13 @@ def cyclic_d100():
     for n in cycle(range(1, 101)):
         yield n
 
+
 practice_die = cyclic_d100()
+
 
 def cyclic_3d100():
     return sum(next(practice_die) for _ in range(3))
+
 
 class PracticeGame:
     def __init__(self, pos_1, pos_2):
@@ -41,10 +44,11 @@ class PracticeGame:
                     self.win_count[player_turn] += count
                 else:
                     self.live_games[game] += count
-        return min(points_1, points_2)*self.roll_count
+        return min(points_1, points_2) * self.roll_count
+
 
 @lru_cache(None)
-def play_turn(game_state, roll, win_target = 21):
+def play_turn(game_state, roll, win_target=21):
     pos_1, pos_2, points_1, points_2, player_turn, is_won = game_state
     if player_turn == True:
         pos_1 = (pos_1 + roll - 1) % 10 + 1
@@ -63,18 +67,19 @@ def roll_outcomes():
             for k in range(1, 4):
                 yield i + j + k
 
+
 @lru_cache(None)
 def get_new_game_states(game_state: tuple, practice=False):
     if practice:
-        return  (play_turn(game_state, cyclic_3d100(), win_target=1000),)
+        return (play_turn(game_state, cyclic_3d100(), win_target=1000),)
     else:
         new = []
         for roll_outcome in roll_outcomes():
             new.append(play_turn(game_state, roll_outcome))
         return tuple(new)
 
-class DiracGame:
 
+class DiracGame:
     def __init__(self, pos_1, pos_2):
         self.live_games = defaultdict(int)
         self.live_games[(pos_1, pos_2, 0, 0, True, False)] += 1
