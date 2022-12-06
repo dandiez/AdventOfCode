f108c53f2630790e18b58d1f38b046c97bd8f032
dandiez <47832466+dandiez@users.noreply.github.com>
2022-12-06 19:40:10 +0100

    day 6

---
+++ 2022/day_06/solution.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+
+
+def read_input(filename="input"):
+    with open(filename) as f:
+        lines = [line.strip() for line in f.readlines() if line.strip()]
+    return lines[0]
+
+
+def part_1(inp):
+    for i in range(len(inp)):
+        if len(set(inp[i - 4:i])) == 4:
+            return i
+
+
+def part_2(inp):
+    for i in range(len(inp)):
+        if len(set(inp[i - 14:i])) == 14:
+            return i
+
+
+def main(input_file):
+    """Solve puzzle and connect part 1 with part 2 if needed."""
+    # part 1
+    inp = read_input(input_file)
+    p1 = part_1(inp)
+    print(f"Solution to part 1: {p1}")
+
+    # part 2
+    inp = read_input(input_file)
+    p2 = part_2(inp)
+    print(f"Solution to part 2: {p2}")
+    return p1, p2
+
+
+def test_sample_1(self):
+    inp = read_input("sample_1")
+    self.assertEqual(10, part_1(inp))
+
+
+def test_sample_2(self):
+    pass
+
+
+if __name__ == "__main__":
+    print("*** solving tests ***")
+    test_sample_1(TestCase())
+    test_sample_2(TestCase())
+    print("*** solving main ***")
+    main("input")
