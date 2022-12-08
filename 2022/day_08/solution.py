60356b9cfc74462c681b9a32a56c31f866e7d5d9
dandiez <47832466+dandiez@users.noreply.github.com>
2022-12-08 12:05:58 +0100

    day 8 part 2

--- 2022/day_08/solution.py
+++ 2022/day_08/solution.py
@@ -38,7 +38,61 @@ def part_1(inp):
 
 
 def part_2(inp):
-    pass
+    forest = {(x, y): int(inp[y][x]) for x in range(len(inp[0])) for y in range(len(inp))}
+    h = len(inp[0])
+    v = len(inp)
+    max_score = 0
+    for x in range(h):
+        for y in range(v):
+            score = get_score(forest, h, v, (x, y))
+            if score > max_score:
+                print(f"{(x, y)}")
+                max_score = score
+    return max_score
+
+
+def get_score(forest, h, v, pos):
+    x0, y0 = pos
+    if (x0 == 0) or x0 == h - 1 or y0 == 0 or y0 == v - 1:
+        return 0
+    score = 1
+
+    count = 0
+    y = y0
+    for x in range(x0 - 1, -1, -1):
+        count += 1
+        hight = forest[x, y]
+        if hight >= forest[pos]:
+            break
+    score *= count
+
+    count = 0
+    y = y0
+    for x in range(x0 + 1, h):
+        count += 1
+        hight = forest[x, y]
+        if hight >= forest[pos]:
+            break
+    score *= count
+
+    count = 0
+    x = x0
+    for y in range(y0 - 1, -1, -1):
+        count += 1
+        hight = forest[x, y]
+        if hight >= forest[pos]:
+            break
+    score *= count
+
+    count = 0
+    x = x0
+    for y in range(y0 + 1, v):
+        count += 1
+        hight = forest[x, y]
+        if hight >= forest[pos]:
+            break
+    score *= count
+    return score
 
 
 def main(input_file):
@@ -61,7 +115,13 @@ def test_sample_1(self):
 
 
 def test_sample_2(self):
-    pass
+    inp = read_input("sample_1")
+    forest = {(x, y): int(inp[y][x]) for x in range(len(inp[0])) for y in range(len(inp))}
+    h = len(inp[0])
+    v = len(inp)
+    self.assertEqual(8, get_score(forest, h, v, (2, 3)))
+    self.assertEqual(4, get_score(forest, h, v, (2, 1)))
+    self.assertEqual(8, part_2(inp))
 
 
 if __name__ == "__main__":
