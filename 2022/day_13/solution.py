9a4f6047103b5995ed6822b356541febc19784ca
dandiez <47832466+dandiez@users.noreply.github.com>
2022-12-13 20:22:01 +0100

    clean a bit

--- 2022/day_13/solution.py
+++ 2022/day_13/solution.py
@@ -9,7 +9,7 @@ from unittest import TestCase
 class State(Enum):
     in_right_order = 1
     not_in_right_order = 2
-    not_known = 3
+    unknown = 3
 
 
 def read_input(filename="input"):
@@ -27,7 +27,7 @@ def _is_left_int_right_list(left, right):
     return isinstance(left, int) and isinstance(right, list)
 
 
-def _int_to_list(value):
+def int_to_list(value):
     if isinstance(value, int):
         return [value]
     return value
@@ -35,44 +35,51 @@ def _int_to_list(value):
 
 def is_in_right_order(left, right) -> State:
     if is_mixed_types(left, right):
-        left = _int_to_list(left)
-        right = _int_to_list(right)
+        left = int_to_list(left)
+        right = int_to_list(right)
     if isinstance(left, int) and isinstance(right, int):
-        if left == right:
-            return State.not_known
-        elif left < right:
-            return State.in_right_order
-        else:
-            return State.not_in_right_order
-    # both are lists
-    state = State.not_known
-    while (state is State.not_known) and left and right:
+        return is_in_right_order_ints(left, right)
+    return is_in_right_order_lists(left, right)
+
+
+def is_in_right_order_lists(left: list, right: list) -> State:
+    state = State.unknown
+    while (state is State.unknown) and left and right:
         l = left.pop(0)
         r = right.pop(0)
         state = is_in_right_order(l, r)
-    if state is not State.not_known:
+    if state is not State.unknown:
         return state
     if not left and not right:
-        return state
+        return State.unknown
     if not left:
         return State.in_right_order
-    elif not right:
-        return State.not_in_right_order
+    return State.not_in_right_order
+
+
+def is_in_right_order_ints(left: int, right: int) -> State:
+    if left == right:
+        return State.unknown
+    elif left < right:
+        return State.in_right_order
+    return State.not_in_right_order
 
 
 def part_1(inp):
     pairs = {i: pair for i, pair in zip(range(1, len(inp) + 1), inp)}
-    return sum(i for i, p in pairs.items() if is_in_right_order(*p) is State.in_right_order)
+    return sum(
+        i for i, p in pairs.items() if is_in_right_order(*p) is State.in_right_order
+    )
 
 
 def part_2(inp):
-    p1 = Packet([[6]])
-    p2 = Packet([[2]])
     packets = []
     for a, b in inp:
         packets.append(a)
         packets.append(b)
     ps = [Packet(p) for p in packets]
+    p1 = Packet([[6]])
+    p2 = Packet([[2]])
     ps.append(p1)
     ps.append(p2)
     ps.sort()
@@ -84,8 +91,10 @@ class Packet:
     p: Any
 
     def __gt__(self, other):
-        if is_in_right_order(
-                copy.deepcopy(self.p), copy.deepcopy(other.p)) is State.in_right_order:
+        if (
+            is_in_right_order(copy.deepcopy(self.p), copy.deepcopy(other.p))
+            is State.in_right_order
+        ):
             return False
         return True
 
