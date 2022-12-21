7c0df6d92acfdbb07d60578017579a7fd3dad207
dandiez <47832466+dandiez@users.noreply.github.com>
2022-12-21 09:08:18 +0100

    day22p2

--- 2022/day_21/solution.py
+++ 2022/day_21/solution.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import dataclasses
 from unittest import TestCase
+from scipy.optimize import fsolve
 
 
 @dataclasses.dataclass
@@ -37,16 +38,43 @@ class Monkey:
 def read_input(filename="input") -> dict[str, Monkey]:
     with open(filename) as f:
         lines = [line.strip() for line in f.readlines() if line.strip()]
-    inp = [Monkey.from_line(val) for val in lines]
-    return {m.name: m for m in inp}
+    return lines
+
+
+@dataclasses.dataclass
+class Monkeys:
+    _monkeys: dict[str, Monkey]
+    _raw: list[str]
+
+    @classmethod
+    def from_inp(cls, inp: list[str]):
+        ms = [Monkey.from_line(val) for val in inp]
+        return cls(
+            _monkeys={m.name: m for m in ms},
+            _raw=inp
+        )
+
+    def p2_reset(self):
+        ms = [Monkey.from_line(val) for val in self._raw]
+        self._monkeys = {m.name: m for m in ms}
+        self._monkeys["root"].operator = "-"
+        self._monkeys["humn"] = Monkey(name="humn")
+
+    def get_root(self, human: int):
+        self.p2_reset()
+        self._monkeys["humn"].result = human[0]
+        return self._monkeys["root"].get_result(self._monkeys)
 
 
 def part_1(inp):
-    return inp["root"].get_result(inp)
+    monkeys = Monkeys.from_inp(inp)
+    return monkeys._monkeys["root"].get_result(monkeys._monkeys)
 
 
 def part_2(inp):
-    pass
+    monkeys = Monkeys.from_inp(inp)
+    h = fsolve(monkeys.get_root, 100000000)
+    return int(h[0])
 
 
 def main(input_file):
@@ -66,13 +94,11 @@ def main(input_file):
 def test_sample_1(self):
     inp = read_input("sample_1")
     self.assertEqual(152, part_1(inp))
-    pass
 
 
 def test_sample_2(self):
-    # inp = read_input("sample_1")
-    # self.assertEqual(1, part_1(inp))
-    pass
+    inp = read_input("sample_1")
+    self.assertEqual(301, part_2(inp))
 
 
 if __name__ == "__main__":
